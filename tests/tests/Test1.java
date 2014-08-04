fd788616393d6e7eddf69cca1c830245929d185f
Eric Bodden <eric@bodden.de>
2014-08-04 16:12:26 +0200

    added some test case

---
+++ tests/tests/Test1.java
@@ -0,0 +1,21 @@
+package tests;
+
+public class Test1 {
+	
+	static class A {
+		void foo() { bar(); }
+		void bar() { }		
+	}
+
+	public static void main(String[] args) {
+		A a1 = new A();
+		a1.foo();
+		
+		A a2 = new A();
+		a2.foo();
+
+		a2.bar();
+
+	}
+
+}
